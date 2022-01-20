325f69f030b60a1e044943c7113c5a66556255a6
Zari Bae <ahmadazzhari@gmail.com>
2022-01-20 20:24:43 +0700

    added paired devices list

--- BluetoothChat/app/src/main/java/com/example/bluetoothchat/MainActivity.java
+++ BluetoothChat/app/src/main/java/com/example/bluetoothchat/MainActivity.java
@@ -1,9 +1,17 @@
 package com.example.bluetoothchat;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +19,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Context context;
+    private BluetoothAdapter bluetoothAdapter;
+    private final int LOCATION_PERMISSION_REQUEST = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         context = this;
+
+        initBluetooth();
+    }
+
+    private void initBluetooth() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(context, "No Bluetooth Found", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void enableBluetooth() {
+        if (bluetoothAdapter.isEnabled()) {
+            Toast.makeText(context, "Bluetooth Already Enabled", Toast.LENGTH_SHORT).show();
+        } else {
+            bluetoothAdapter.enable();
+        }
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST);
+        } else {
+            Intent intent = new Intent(context, DeviceListActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(context, DeviceListActivity.class);
+                startActivity(intent);
+            } else {
+                new AlertDialog.Builder(context)
+                        .setCancelable(false)
+                        .setMessage("Location permission is required.\nPlease grant")
+                        .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                checkPermission();
+                            }
+                        })
+                        .setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                MainActivity.this.finish();
+                            }
+                        }).show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     @Override
@@ -30,10 +94,10 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_search_devices:
-                Toast.makeText(context, "Clicked Search Devices", Toast.LENGTH_SHORT).show();
+                checkPermission();
                 return true;
             case R.id.menu_enable_bluetooth:
-                Toast.makeText(context, "Clicked Enable Bluetooth", Toast.LENGTH_SHORT).show();
+                enableBluetooth();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
